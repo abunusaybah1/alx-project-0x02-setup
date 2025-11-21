7899d0bd9d145e567454565bebd51d8d7efdd8c6
abunusaybah <your_github_email@example.com>
2025-11-21 12:04:03 +0100

    Completed task 3

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,13 @@
+import { CardProps } from "@/interfaces";
+import React from "react";
+
+const Card: React.FC<CardProps> = ({ title, content }) => {
+	return (
+		<div>
+			<h2>{title}</h2>
+			<p>{content}</p>
+		</div>
+	);
+};
+
+export default Card;
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import Card from "@/components/common/Card";
+import React from "react";
 
 const Home: React.FC = () => {
-  return (
-      <div>
-          <h2>Home Page</h2>
-    </div>
-  )
-}
+	return (
+		<div>
+			<h2>Home Page</h2>
+			<Card
+				title="News"
+				content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique necessitatibus voluptas dolores quas nam error cumque iusto hic dolore accusamus, animi vitae nulla commodi ea vel temporibus eaque iste quos."
+			/>
+			<Card
+				title="Sports"
+				content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique necessitatibus voluptas dolores quas nam error cumque iusto hic dolore accusamus, animi vitae nulla commodi ea vel temporibus eaque iste quos."
+			/>
+			<Card
+				title="Education"
+				content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique necessitatibus voluptas dolores quas nam error cumque iusto hic dolore accusamus, animi vitae nulla commodi ea vel temporibus eaque iste quos."
+			/>
+			<Card
+				title="Tech"
+				content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique necessitatibus voluptas dolores quas nam error cumque iusto hic dolore accusamus, animi vitae nulla commodi ea vel temporibus eaque iste quos."
+			/>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
